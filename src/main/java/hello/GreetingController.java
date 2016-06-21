205adb4cdaa465379db6d2bade7ca589ed1cfecd
Jacinda <jacindaz@gmail.com>
2016-06-21 14:32:03 -0400

    Comments in GreetingController to explain what things mean and Rails equivalent

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -8,8 +8,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
+    @RequestMapping("/greeting", method=GET)
+    // @RequestMapping maps all HTTP operations by default
+    // could do @RequestMapping("/greeting", method=GET) to be more explicit
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+
+        // value="name" binds the "name" key from params to this method
+        // must be present, otherwise the default will be "World"
+        // kinda like strong params in Rails
+
+        // adding "name" to the Model object makes it accessible to the view
+        // (so maybe like an instance var in Rails in the controller?)
         model.addAttribute("name", name);
         return "greeting";
     }

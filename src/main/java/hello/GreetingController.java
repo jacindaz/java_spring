f88220350f9db832ff3b6c59686cda15a3a3c798
Jacinda <jacindaz@gmail.com>
2016-06-22 11:43:31 -0400

    More comments about the 'view model'

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting", method=GET)
+    @RequestMapping("/greeting")
     // @RequestMapping maps all HTTP operations by default
     // could do @RequestMapping("/greeting", method=GET) to be more explicit
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
@@ -17,6 +17,7 @@ public class GreetingController {
         // must be present, otherwise the default will be "World"
         // kinda like strong params in Rails
 
+        // view model, where all needed attributes added to the "Model" object
         // adding "name" to the Model object makes it accessible to the view
         // (so maybe like an instance var in Rails in the controller?)
         model.addAttribute("name", name);
